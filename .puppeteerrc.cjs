fa46277b54d0e9d843a2ae7298e2383fdca54979
te-ce <9517685+te-ce@users.noreply.github.com>
2025-06-27 15:22:17 +0200

    init

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+// eslint-disable-next-line no-undef
+module.exports = {
+  chrome: {
+    skipDownload: false,
+  },
+  firefox: {
+    skipDownload: false,
+  },
+  defaultBrowser: "chrome",
+};
